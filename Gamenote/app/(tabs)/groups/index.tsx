a887f3ac01290e69cc0c80989c2b74907a49627a
Mario Barisa <mario@barisa.me>
2026-04-11 13:41:41 +0200

    minimalni bug fixevi

--- Gamenote/app/(tabs)/groups/index.tsx
+++ Gamenote/app/(tabs)/groups/index.tsx
@@ -1,11 +1,11 @@
-import {Text, ScrollView, StyleSheet, TextInput, View, Button, Alert} from "react-native";
-import { useState } from "react";
-import { useFavorites } from "@/context/favorites";
+// noinspection JSUnusedGlobalSymbols
+import { ScrollView } from "react-native";
 import { useTheme } from "@/context/theme";
 import { colors } from "@/constants/theme";
 
 
-export default function FavoritesScreen() {
+// noinspection JSUnusedGlobalSymbols
+export default function() {
 
   const { theme } = useTheme();
   const t = colors[theme];
@@ -20,47 +20,3 @@ export default function FavoritesScreen() {
 }
 
 
-const styles = StyleSheet.create({
-  name: {
-    fontSize: 20,
-    fontWeight: "bold",
-    textAlign: "center"
-  },
-
-  text:{
-    fontSize: 20,
-    paddingTop: 50,
-    paddingBottom: 5,
-    fontWeight: "bold",
-    textAlign: "center"
-  },
-    textBody:{
-    fontSize: 15,
-    padding: 5,
-    fontWeight: "600", //semibold == 600
-    alignItems: "flex-start"
-  },
-
-  textMain:{
-    fontSize: 35,
-    padding: 15,
-    fontWeight: "bold",
-    textAlign: "center"
-  },
-
-    textBodyCenterHiglighted: {
-      fontSize: 15,
-      fontWeight: "bold",
-      textAlign: "center"
-    },
-
-    systemInput: {
-      backgroundColor: "rgba(118, 118, 128, 0.12)",
-      borderRadius: 10,
-      paddingHorizontal: 8,
-      paddingVertical: 7,
-      fontSize: 17,
-      marginHorizontal: 16,
-    },
-
-});
