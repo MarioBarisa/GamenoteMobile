7661887e69ff206727df434f84181ad96ece0d99
Mario Barisa <mario@barisa.me>
2026-04-23 13:53:24 +0200

    Dodani gumb za toggle između korisnikovog Gamenote-a i globalne Gamenote baze prilikom pretrage te gumb za sortiranje. Potrebna implementacija

--- Gamenote/app/(tabs)/search/index.tsx
+++ Gamenote/app/(tabs)/search/index.tsx
@@ -1,14 +1,17 @@
 import { useMemo, useState } from "react";
-import { ScrollView } from "react-native";
+import {ScrollView, Text, TouchableOpacity, Alert} from "react-native";
 import { Stack } from "expo-router";
 import { useTheme } from "@/context/theme";
 import { colors } from "@/constants/theme";
 import GameCard, {Game} from "@/components/GameCard";
 import {PLACEHOLDER_GAMES} from "@/constants/PLACEHOLDER_GAMES";
+import { SymbolView } from "expo-symbols";
 
 // noinspection JSUnusedGlobalSymbols
 export default function SearchIndex() {
   const [search, setSearch] = useState("");
+  const [userGamenotesOnly, setUserGamenotesOnly] = useState(false);
+  const [sort, setSort] = useState(false);
   const PLACEHOLDER_IGRE = PLACEHOLDER_GAMES;
 
   const filteredGames = useMemo(() => {
@@ -26,6 +29,33 @@ export default function SearchIndex() {
       <Stack.Screen
         options={{
           title: "Pretraži Gamenote",
+          headerRight: () => (
+            <TouchableOpacity
+              onPress={() => setSort(prev => !prev)}
+              onLongPress={() => Alert.alert("Sortiranje", "Sortiraj rezultate pretrage. Igre koje najviše igraš su prve.")}
+              style={{ justifyContent: 'center', alignItems: 'center'}}>
+             <SymbolView
+                 key={sort ? "line.horizontal.3.decrease.circle" : "line.horizontal.3.decrease.circle.fill"}
+                 name={sort ? "line.horizontal.3.decrease.circle" : "line.horizontal.3.decrease.circle.fill"}
+                resizeMode="scaleAspectFit"
+                style={{ width: 36, height: 30 }}
+              />
+            </TouchableOpacity>
+          ),
+          headerLeft: () => (
+            <TouchableOpacity
+              onPress={() => setUserGamenotesOnly(prev => !prev)}
+              onLongPress={() => Alert.alert("Tvoji Gamenote", "Ovaj gumb uključuje filtiranje prikaza samo na tvoje dodane igre. Inače tražiš cijeli Gamenote database.")}
+              style={{ justifyContent: 'center', alignItems: 'center'}}>
+
+              <SymbolView
+                key={userGamenotesOnly ? "bookmark.fill" : "bookmark"}
+                name={userGamenotesOnly ? "bookmark.fill" : "bookmark"}
+                resizeMode="scaleAspectFit"
+                 style={{ width: 36, height: 30 }}
+              />
+            </TouchableOpacity>
+          ),
           headerSearchBarOptions: {
             placeholder: "Pretraži Gamenote",
             onChangeText: (event) => setSearch(event.nativeEvent.text),
@@ -44,4 +74,3 @@ export default function SearchIndex() {
     </>
   );
 }
-
